c9d2f2ceed6eb6f1bd2518999461ddbbb135ffeb
David <davejohngoddard@gmail.com>
2025-02-12 01:40:17 +0000

    Added application insights to java app

--- src/main/java/com/nomad/backend/BackendApplication.java
+++ src/main/java/com/nomad/backend/BackendApplication.java
@@ -1,23 +1,15 @@
 package com.nomad.backend;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.neo4j.cypherdsl.core.renderer.Configuration;
-import org.neo4j.cypherdsl.core.renderer.Dialect;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.neo4j.core.Neo4jClient;
-import org.springframework.data.neo4j.core.ReactiveNeo4jClient;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-//		some change
+		// Note; the application insights connectionstring is set as an environment variable in the web app.
+		ApplicationInsights.attach();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
