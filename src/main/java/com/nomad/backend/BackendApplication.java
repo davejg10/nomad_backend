33dae774f1893a08db5c57fdea9647597ae89518
David <davejohngoddard@gmail.com>
2025-02-12 16:35:56 +0000

    Added spring profiles

--- src/main/java/com/nomad/backend/BackendApplication.java
+++ src/main/java/com/nomad/backend/BackendApplication.java
@@ -4,12 +4,17 @@ import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		// Note; the application insights connection string is set as an `APPLICATIONINSIGHTS_CONNECTION_STRING` environment variable.
-		ApplicationInsights.attach();
+		String profile = System.getProperty("spring.profiles.active", "local");
+		if (!Objects.equals(profile, "local")) {
+			// Note; the application insights connection string is set as `APPLICATIONINSIGHTS_CONNECTION_STRING` environment variable.
+			ApplicationInsights.attach();
+		}
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
