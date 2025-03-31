c080e1a214b50505ca48b1cf9fdd392722d7c5d8
David <davejohngoddard@gmail.com>
2025-03-31 16:03:26 +0100

    Pulling in with some annotaitons

--- src/main/java/com/nomad/backend/BackendApplication.java
+++ src/main/java/com/nomad/backend/BackendApplication.java
@@ -5,19 +5,25 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Objects;
 
 @Log4j2
 @SpringBootApplication
 @EntityScan(basePackages = {"com.nomad.data_library.domain"})
+@EnableJpaRepositories({"com.nomad.data_library.repositories", "com.nomad.backend.routes"})
+@ComponentScan({"com.nomad.data_library.config", "com.nomad.backend"})
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		Environment environment = SpringApplication.run(BackendApplication.class, args).getEnvironment();
 
 		String profile = environment.getProperty("spring.profiles.active", "local");
+
+		log.info("Spring profile is {}", profile);
 
 		if (!Objects.equals(profile, "local")) {
 			log.info("Attaching application insights");
