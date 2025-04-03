17acb25b8f1ae254ff7edd21a7bf66b63d3742b2
David <davejohngoddard@gmail.com>
2025-04-03 09:22:06 +0100

    Changed to central log4j files

--- src/main/java/com/nomad/backend/BackendApplication.java
+++ src/main/java/com/nomad/backend/BackendApplication.java
@@ -1,13 +1,10 @@
 package com.nomad.backend;
 
-import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import lombok.extern.log4j.Log4j2;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.core.env.Environment;
-
-import java.util.Objects;
 
 @Log4j2
 @SpringBootApplication
@@ -15,15 +12,8 @@ import java.util.Objects;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Environment environment = SpringApplication.run(BackendApplication.class, args).getEnvironment();
+		SpringApplication.run(BackendApplication.class, args);
 
-		String profile = environment.getProperty("spring.profiles.active", "local");
-		
-		if (!Objects.equals(profile, "local")) {
-			log.info("Attaching application insights");
-			// Note; the application insights connection string is set as `APPLICATIONINSIGHTS_CONNECTION_STRING` environment variable.
-			ApplicationInsights.attach();
-		}
 	}
 
 }
